bccea37fed5fefecbb28981f351a748897154083
JernejL123 <jernej.lobnik@gmail.com>
2024-11-24 15:10:20 +0100

    dodal loginpage (do neke mere)

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,13 +1,35 @@
 import NavBar from '../Components/NavBar';
 import Footer from '../Components/Footer';
+import loginICON from '../LoginPage/LoginPageAssets/login-3-svgrepo-com.svg'
+import './LoginPage.css'
 
 function LoginPage(){
     return(
-        <div>
+        <>
             <NavBar/>
 
+            <main>
+                <div className='login-container'>
+                    <div className='login-top'>
+                        <img src={loginICON} alt='login_icon' width='60px' height='60px'/>
+                    </div>
+
+                    <div className='login-mid'>
+                        <input className='input-text-field' type='text' placeholder="Username"/>
+                        <input className='input-text-field' type='text' placeholder="Password"/>
+                        <button className='login-button'><b>LOG IN</b></button>
+                        <hr></hr>
+                    </div>
+
+                    <div className='login-bot'>
+
+                    </div>
+                </div>
+            </main>
+
             <Footer/>
-        </div>
+        </>
     )
 }
+
 export default LoginPage
