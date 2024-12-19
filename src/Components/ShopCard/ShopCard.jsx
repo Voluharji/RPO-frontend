d60af2e161fcfd50b82b73d6255d8c180f47e9c5
Jackob365 <jakob.rencelj@gmail.com>
2024-12-19 21:00:11 +0100

    idk kaj se dogaja

--- src/Components/ShopCard/ShopCard.jsx
+++ src/Components/ShopCard/ShopCard.jsx
@@ -1,24 +1,20 @@
 import CartSvg from './ShopCardAssets/shopping-cart-outline-svgrepo-com.svg'
 import './ShopCard.css'
-import {useNavigate} from "react-router-dom";
 
-
-function ShopCard({name,price,photo,id}){
-
-    const navigate = useNavigate();
-    const handleCardClick = () => {
-        navigate(`/ShopPage/${id}`);
-    };
+function ShopCard({name,price,description,photo}){
 
     return(
-        <div className='shop-card'  onClick={handleCardClick}>
+        <div className='shop-card'>
             <div className='shop-card-photo'>
                 <img src={photo} alt='img' height='100%' width='100%'/>
             </div>
             <div className='shop-card-info'>
+                <br/>
                 <h3 className='shop-card-name'>{name}</h3>
+                <br/>
                 <p className='price-text'><b>{price}</b></p>
-                <button className='shop-card-btn'><img src={CartSvg} alt='Cart' height='100%' width='100%'/></button>
+                <br/>
+                <p>{description}</p>
             </div>
         </div>
     )
