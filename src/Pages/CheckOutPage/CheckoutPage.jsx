40bc2b5d4ba6f0aabc129c07a857c62da3cd853b
JernejL123 <jernej.lobnik@gmail.com>
2025-01-17 23:53:05 +0100

    polepsan checkout

--- src/Pages/CheckOutPage/CheckoutPage.jsx
+++ src/Pages/CheckOutPage/CheckoutPage.jsx
@@ -5,14 +5,14 @@ import Box from "@mui/material/Box";
 import "./CheckoutPage.css";
 import CreditScoreOutlinedIcon from "@mui/icons-material/CreditScoreOutlined";
 import EuroOutlinedIcon from "@mui/icons-material/EuroOutlined";
-import ToggleButton from "@mui/material/ToggleButton";
 import { Button } from "@mui/material";
 import { useState } from "react";
 
 import logoSVG from '../../Components/NavBar/NavBarAssets/VoSuHi.svg'
 
 function CheckoutPage() {
-    const [selectedToggle, setSelectedToggle] = useState("pickup");
+    const [selectedToggle, setSelectedToggle] = useState("delivery");
+
 
     const handleToggle = (value) => {
         setSelectedToggle(value);
@@ -31,64 +31,63 @@ function CheckoutPage() {
                         width: "47%",
                         gap: 2,
                         margin: 2,
+                        opacity: '1'
 
                     }}
                     noValidate
                     autoComplete="off"
                 >
-                    {/* Toggle buttons for Delivery and Pick Up */}
-                    <ToggleButton
+
+                    <Button
                         value="delivery"
                         selected={selectedToggle === "delivery"} // Check if this is selected
                         onClick={() => handleToggle("delivery")} // Toggle state on click
                         sx={{
-                            color: selectedToggle === "delivery" ? "rgb(204,134,5)" : "rgb(204,134,5)",
-                            backgroundColor: selectedToggle === "delivery" ? "rgb(204,134,5)" : "rgba(204,134,5,0.22)",
-                            borderColor: "rgb(197,129,0)",
+                            color: selectedToggle === "delivery" ? "white" : "black",
+                            backgroundColor: selectedToggle === "delivery" ? "rgb(0,0,0,1)" : "rgb(255,255,255)",
+                            border: "none"
                         }}
                     >
                         <h3>DELIVERY</h3>
-                    </ToggleButton>
-                    <ToggleButton
+                    </Button>
+                    <Button
                         value="pickup"
                         selected={selectedToggle === "pickup"} // Check if this is selected
                         onClick={() => handleToggle("pickup")} // Toggle state on click
                         sx={{
-                            color: selectedToggle === "pickup" ? "white" : "rgb(204,134,5)",
-                            backgroundColor: selectedToggle === "pickup" ? "rgb(204,134,5)" : "rgba(204,134,5,0.22)",
-                            borderColor: "rgb(197,129,0)",
+                            color: selectedToggle === "pickup" ? "white" : "black",
+                            backgroundColor: selectedToggle === "pickup" ? "rgb(0,0,0)" : "rgb(255,255,255)",
+                            border: "none"
                         }}
                     >
                         <h3>PICK UP</h3>
-                    </ToggleButton>
+                    </Button>
 
                     <hr/>
                     <hr/>
 
-                    <TextField id="outlined-basic" label="Cardholder name" variant="outlined"/>
-                    <TextField id="outlined-basic" label="Cardholder surname" variant="outlined"/>
+                    <CountrySelect sx={{width: "60%", border: "none"}}/>
+                    <TextField id="outlined-basic" label="Phone number" variant="filled"/>
 
-                    <TextField id="outlined-basic" label="Billing address" variant="outlined"/>
-                    <TextField id="outlined-basic" label="Post number" variant="outlined"/>
+                    <TextField id="outlined-basic" label="Cardholder name" variant="filled"/>
+                    <TextField id="outlined-basic" label="Cardholder surname" variant="filled"/>
 
-                    <TextField id="outlined-basic" label="Phone number" variant="outlined"/>
-                    <CountrySelect sx={{width: "60%"}}/>
-
-                    <hr/>
-                    <hr/>
+                    <TextField id="outlined-basic" label="Billing address" variant="filled"/>
+                    <TextField id="outlined-basic" label="Post number" variant="filled"/>
 
                     <CreditScoreOutlinedIcon fontSize="medium"/>
                     <EuroOutlinedIcon fontSize="medium"/>
 
-                    <TextField id="outlined-basic" label="Card number" variant="outlined"/>
-                    <TextField id="outlined-basic" label="Expiration date" variant="outlined"/>
-                    <TextField id="outlined-basic" label="CVC" variant="outlined"/>
+                    <TextField id="outlined-basic" label="Card number" variant="filled"/>
+                    <TextField id="outlined-basic" label="Expiration date" variant="filled"/>
+                    <TextField id="outlined-basic" label="CVC" variant="filled"/>
 
                     <Button
                         variant="outlined"
                         sx={{
-                            color: "rgb(204,134,5)",
-                            borderColor: "rgb(197,129,0)",
+                            color: "white",
+                            border: "none",
+                            backgroundColor: "black"
                         }}
                     >
                         <h3>PURCHASE</h3>
